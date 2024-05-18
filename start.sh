fdc1c114532d13e96e247b3d1ce11b16c281e0a8
Yczbot <yugmalviya35@gmail.com>
2024-05-18 20:49:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/Yczbot/Advance-Mario-Share-Bot /Advance-Mario-Share-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /Advance-Mario-Share-Bot
 fi
 cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
