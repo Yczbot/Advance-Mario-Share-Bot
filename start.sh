01a3e3463ee2de56fd52f06ecec26fac1e678561
Yczbot <yugmalviya35@gmail.com>
2024-05-18 20:58:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Advance-Mario-Share-Bot
 fi
-cd /Advance-File-Share-Bot
+cd /Advance-Mario-Share-Bot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
